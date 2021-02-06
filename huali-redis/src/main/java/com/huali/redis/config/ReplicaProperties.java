502c02e7ccf8576ac89da8f9a9f18fb7ef178083
杨明阳 <a507984632@163.com>
2021-02-06 16:18:10 +0800

    添加对 @ConfigurationProperties 注解的理解 且添加 spring-boot-configuration-processor 配置文件处理器 的 pom 坐标

--- huali-redis/src/main/java/com/huali/redis/config/ReplicaProperties.java
+++ huali-redis/src/main/java/com/huali/redis/config/ReplicaProperties.java
@@ -13,6 +13,15 @@ import java.util.Set;
  **/
 @Data
 @Configuration
+/**
+ *  @ConfigurationProperties
+ *  从 application.yml 中读取 spring.redis.replica 下所有的信息 并注入到这个类中
+ *  prefix：前缀
+ *  ignoreInvalidFields：忽略无效字段
+ *  ignoreUnknownFields：忽略未知字段
+ *
+ *  添加了这个注解的类应该在 SpringBoot 扫描包下
+ */
 @ConfigurationProperties(prefix = "spring.redis.replica")
 public class ReplicaProperties {
 
