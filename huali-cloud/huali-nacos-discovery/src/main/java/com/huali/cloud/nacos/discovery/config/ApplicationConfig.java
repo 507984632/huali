24bdae8f7ed8a0a1eb207112c8b861f75f61e5a4
杨明阳 <a507984632@163.com>
2021-03-11 16:41:17 +0800

    添加 SpringCloud 模块，该模块相对独立，      内容有 nacos 的服务中心 和 配置中心的实现

---
+++ huali-cloud/huali-nacos-discovery/src/main/java/com/huali/cloud/nacos/discovery/config/ApplicationConfig.java
@@ -0,0 +1,26 @@
+package com.huali.cloud.nacos.discovery.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Nacos 中关于 远程调用，负载均衡的配置
+ *
+ * @author Yang_my
+ * @since 2021/3/8
+ */
+@Configuration
+public class ApplicationConfig {
+    /**
+     * -@LoadBalanced：OpenFen 中关于负载均衡 的配置
+     *
+     * @return .
+     */
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
